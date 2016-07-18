367d52a967b565dbbdf06d328d51ee217d7a871d
Daniel E Lanier <lanier4@illinois.edu>
2016-07-18 16:11:40 -0500

    production version the first

--- NBS.py
+++ NBS.py
@@ -4,48 +4,36 @@ Created on Tue Jun 21 11:48:41 2016
 @author: dlanier
 @author: nahilsobh
 
+
 This script performs network based clustering
 """
 
 from knpackage import toolbox as kn
-import scipy.sparse as spar
-from numpy import maximum
-  
+import sys
+
 def main():
-    '''Performs network based clustering'''
-    # ---------------------------------
-    # Input, Clean, and set parameters
-    # ---------------------------------
-    network, spreadsheet, lut = kn.get_input()
-    network_sparse = spar.csr_matrix(network)
-    Ld, Lk = kn.form_network_laplacian(network)
-    percent_sample, number_of_samples = kn.parameters_setup()
-    network_size = network.shape[0]
-    connectivity_matrix, indicator_matrix = kn.initialization(spreadsheet)
-
-    # ----------------------------------------------
-    # Network based clustering loop and aggregation
-    # ----------------------------------------------
-    for sample in range(0, number_of_samples):
-        sample_random, sample_permutation = kn.get_a_sample(spreadsheet, percent_sample,
-                                                         lut, network_size)
-        sample_smooth, iterations = kn.rwr(sample_random, network_sparse, alpha=0.7)
-        print("iterations = ", iterations)
-        sample_quantile_norm = kn.quantile_norm(sample_smooth)
-        H, niter = kn.netnmf(sample_quantile_norm, Lk, Ld, k=3)
-        connectivity_matrix = kn.update_connectivity_matrix(H, sample_permutation, connectivity_matrix)
-        indicator_matrix = kn.update_indicator_matrix(sample_permutation, indicator_matrix)
-        
-    # --------------------------
-    # From the consensus matrix
-    # --------------------------
-    consensus_matrix = connectivity_matrix / maximum(indicator_matrix, 1)
-    M = kn.reorder_matrix(consensus_matrix)
+    """ main function for finding clustering by network based stratification 
+        with consensus clustering
     
-    # --------------------
-    # Display the results
-    # --------------------
-    kn.display_clusters(M)
-
+    Args:
+        defaults: knpackage.toolbox.nbs_par_set_dict
+        args='-parameters /Users/del/tempOLE/keg_nbs_luad_h90q.df'
+        args='-parameters /Users/del/tempOLE/keg_nbs_ov_h90q.df'
+        args='-parameters /Users/del/tempOLE/keg_nbs_ucec_st90q.df'
+    Returns:
+        nothing
+        
+    """
+    adj_mat, spreadsheet, par_set_dict, sample_names = kn.get_input(sys.argv)
+    Ld, Lk = kn.form_network_laplacian(adj_mat)
+    kn.form_and_save_h_clusters(adj_mat, spreadsheet, Ld, Lk, par_set_dict)
+    connectivity_matrix, indicator_matrix = kn.initialization(spreadsheet)
+    consensus_matrix = kn.retrieve_h_clusters_and_form_conensus_matrix(
+        par_set_dict, connectivity_matrix, indicator_matrix)
+    M, labels = kn.reorder_matrix(consensus_matrix, int(par_set_dict['k']))
+    if int(par_set_dict['display_clusters']) != 0:
+        kn.display_clusters(M)
+    kn.write_sample_labels(sample_names, labels, par_set_dict['consensus_data_filename'])
+    
 if __name__ == "__main__":
     main()
