6e5c071ec5228eb9233b200b76bbc1f1f70ab0c4
Daniel E Lanier <lanier4@illinois.edu>
2016-07-20 12:50:18 -0500

    changed calls to toolbox to accomodate function changes

--- NBS.py
+++ NBS.py
@@ -1,8 +1,8 @@
 """
 Created on Tue Jun 21 11:48:41 2016
 
-@author: dlanier
 @author: nahilsobh
+@author: dlanier
 
 This script performs network based clustering
 """
@@ -28,16 +28,18 @@ def main():
     Ld, Lk = kn.form_network_laplacian(adj_mat)
     kn.form_and_save_h_clusters(adj_mat, spreadsheet, Ld, Lk, par_set_dict)
     connectivity_matrix, indicator_matrix = kn.initialization(spreadsheet)
-    consensus_matrix = kn.htemps_to_consensusmatrix(
+    consensus_matrix = kn.read_h_clusters_to_consensus_matrix(
         par_set_dict, connectivity_matrix, indicator_matrix)
-    M, labels = kn.reorder_matrix(consensus_matrix, int(par_set_dict['k']))
-    if int(par_set_dict['display_clusters']) != 0:
-        kn.display_clusters(M)
+    labels = kn.get_labels(consensus_matrix, int(par_set_dict['k']))
     
     kn.write_output(consensus_matrix, sample_names, labels,
                     par_set_dict['consensus_data_df'])
     kn.write_sample_labels(sample_names, labels,
                            par_set_dict['consensus_data_tsv'])
+                           
+    if int(par_set_dict['display_clusters']) != 0:
+        M = kn.reorder_matrix(consensus_matrix, int(par_set_dict['k']))
+        kn.display_clusters(M)
     
 if __name__ == "__main__":
     main()
