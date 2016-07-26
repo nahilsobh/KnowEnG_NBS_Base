2523946a2b1830939b395944d9f7e2549f5d079e
Daniel E Lanier <lanier4@illinois.edu>
2016-07-26 14:22:53 -0500

    update required by git logic - no actual changes

--- main.py
+++ main.py
@@ -10,33 +10,33 @@ def nmf(run_parameters):
     '''nmf clustering'''
     from knpackage.toolbox import run_nmf
     run_nmf(run_parameters) 
-    
+
 def cc_nmf(run_parameters):
     '''kmeans consensus clustering of the nmf-based clusters'''
     from knpackage.toolbox import run_cc_nmf
     run_cc_nmf(run_parameters)
-    
+
 def net_nmf(run_parameters):
     '''net-nmf clustering "'''
     from knpackage.toolbox import run_net_nmf
     run_net_nmf(run_parameters)
-    
+
 def cc_net_nmf(run_parameters):
     '''kmeans consensus clustering of the net-nmf-based clusters'''
     from knpackage.toolbox import run_cc_net_nmf
     run_cc_net_nmf(run_parameters)
-    
+
 SELECT = {
     "nmf":nmf,
     "cc_nmf":cc_nmf,
     "net_nmf":net_nmf,
     "cc_net_nmf":cc_net_nmf}
-    
+
 def main():
     import sys
     from knpackage.toolbox import get_input
     from knpackage.toolbox import get_run_parameters
-    
+
     run_directory = get_input(sys.argv)
     run_file = "run_file"
     run_parameters = get_run_parameters(run_file,run_directory)
